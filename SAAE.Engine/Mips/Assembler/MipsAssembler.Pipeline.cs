080f64561b048d91ef6304169d6e7c0dc84ea4e4
Rodrigo Appelt <rappelt@inf.ufsm.br>
2024-06-15 10:38:36 -0300

    pipeline part 1

---
+++ SAAE.Engine/Mips/Assembler/MipsAssembler.Pipeline.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAAE.Engine.Mips.Assembler; 
+
+public class SourceTextStage {
+    public List<string> SourceText { get; init; } = [];
+}
+
+public class MacroSolvingStage {
+    public List<string> SourceText { get; init; } = [];
+}
+
+public class PseudoInstructionSolvingStage {
+    public List<string> SourceText { get; init; } = [];
+}
+
+public class SymbolReadingStage {
+    public List<string> SourceText { get; init; } = [];
+
+    public Dictionary<string, int> SymbolTable { get; init; } = [];
+}
+
+public class AssembleStage {
+
+    public byte[] DataSegment { get; init; } = [];
+
+    public byte[] TextSegment { get; init; } = [];
+}
--- SAAE.Engine/Mips/Assembler/MipsAssembler.cs
+++ SAAE.Engine/Mips/Assembler/MipsAssembler.cs
@@ -1,9 +1,5 @@
 ﻿using SAAE.Engine.Mips.Instructions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SAAE.Engine.Mips.Assembler; 
 public partial class MipsAssembler {
@@ -22,6 +18,169 @@ public partial class MipsAssembler {
             "zero", "at", "v0", "v1", "a0", "a1", "a2", "a3", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra"
         ];
         return Array.IndexOf(names, name);
+    }
+
+    public AssembleStage NewAssemble(string code) {
+        var sourceTextStage = InputText(code);
+
+        var macroSolvingStage = ResolveMacros(sourceTextStage);
+
+        var pseudoSolvingStage = ResolvePseudoInstructions(macroSolvingStage);
+
+        var readsymbolsStage = ReadSymbols(pseudoSolvingStage);
+
+        var assembleStage = AssembleInstructions(readsymbolsStage);
+
+
+        return assembleStage;
+    }
+
+    public SourceTextStage InputText(string code) {
+        return new() {
+            SourceText = code
+                .Split('\n')
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                //.Select(x => x.Trim())
+                .ToList()
+        };
+    }
+
+    private class Macro {
+        public string Name { get; init; } = "";
+        public string[] Arguments { get; init; } = [];
+        public List<string> Body { get; init; } = [];
+    }
+
+    public MacroSolvingStage ResolveMacros(SourceTextStage upstream) {
+        List<Macro> macros = [];
+
+        // read macros and remove from source code
+        int state = 0;
+        // 0 - normal
+        // 1 - reading macro
+        Macro? currentMacro = null;
+        for (int i = 0; i < upstream.SourceText.Count; i++) {
+            string line = upstream.SourceText[i];
+            int macroDefinitionIndex = line.IndexOf(".macro");
+            if (macroDefinitionIndex != -1 && state == 0) { // start of macro
+                state = 1;
+                int parenthesisIndex = line.IndexOf('(');
+                if(parenthesisIndex == -1) {
+                    // no arguments
+                    currentMacro = new Macro() {
+                        Name = line[(macroDefinitionIndex + 7)..].Trim()
+                    };
+                } else {
+                    line = line.Replace(')', ' ');
+                    // has arguments
+                    currentMacro = new Macro() {
+                        Name = line[(macroDefinitionIndex + 7)..parenthesisIndex].Trim(),
+                        Arguments = line[(parenthesisIndex + 1)..^1].Split(',').Select(x => x.Trim()).ToArray()
+                    };
+                }
+                upstream.SourceText.RemoveAt(i);
+                i--;
+                continue;
+            } else if (line.Contains(".end_macro") && state == 1) { // macro end
+                macros.Add(currentMacro!);
+                currentMacro = null;
+                upstream.SourceText.RemoveAt(i);
+                i--;
+                state = 0;
+            } else if(state == 1) { // we are reading a macro
+                currentMacro!.Body.Add(line.TrimEnd());
+                upstream.SourceText.RemoveAt(i);
+                i--;
+            }
+            // not macro related
+        }
+
+        // codigo muito feio, talvez refatorar isso depois :(
+        // apply macros
+        for(int i=0; i<upstream.SourceText.Count; i++) {
+            string line = upstream.SourceText[i].TrimStart();
+            foreach (var macro in macros) {
+                if (line.StartsWith(macro.Name)) {
+                    line = line.Split('#')[0].Replace('(', ' ').Replace(')', ' ').Replace(',', ' ');
+                    string[] parts = line.Split(' ', StringSplitOptions.TrimEntries|StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length - 1 != macro.Arguments.Length) {
+                        throw new Exception($"Macro {macro.Name} expects {macro.Arguments.Length} arguments, but {parts.Length - 1} were given.\n" +
+                            $"L{i}: {line}\n" +
+                            $"Arguments: {string.Join(",", parts)}");
+                    }
+                    List<string> newLines = [];
+                    for (int j = 0; j < macro.Body.Count; j++) {
+                        string newLine = macro.Body[j];
+                        for (int k = 0; k < macro.Arguments.Length; k++) {
+                            newLine = newLine.Replace(macro.Arguments[k], parts[k + 1]);
+                        }
+                        newLines.Add(newLine);
+                    }
+                    upstream.SourceText.RemoveAt(i);
+                    upstream.SourceText.InsertRange(i, newLines);
+                    i += newLines.Count - 1;
+                }
+            }
+        }
+
+        // read eqvs
+        Dictionary<string, string> eqvs = [];
+        for (int i = 0; i < upstream.SourceText.Count; i++) {
+            string line = upstream.SourceText[i];
+            int eqvIndex = line.IndexOf(".eqv");
+            if (eqvIndex != -1) {
+                line = line[(eqvIndex + 4)..].Trim();
+                string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                eqvs[parts[0]] = parts[1];
+                upstream.SourceText.RemoveAt(i);
+                i--;
+            }
+        }
+
+        // apply eqvs
+        for (int i = 0; i < upstream.SourceText.Count; i++) {
+            string line = upstream.SourceText[i];
+            foreach (var eqv in eqvs) {
+                line = line.Replace(eqv.Key, eqv.Value);
+            }
+            upstream.SourceText[i] = line;
+        }
+
+        return new() {
+            SourceText = upstream.SourceText
+        };
+    }
+
+    public PseudoInstructionSolvingStage ResolvePseudoInstructions(MacroSolvingStage upstream) {
+        // do nothing 
+        return new() {
+            SourceText = upstream.SourceText
+        };
+    }
+
+    [GeneratedRegex(@"^(?<!#)\s*(?<label>[^\s#]+)\s*(?=:)")]
+    private partial Regex LabelRegex();
+    public SymbolReadingStage ReadSymbols(PseudoInstructionSolvingStage upstream) {
+        Regex labelRegex = LabelRegex();
+        Dictionary<string, int> symbolTable = [];
+        for (int i=0; i< upstream.SourceText.Count; i++) {
+            string line = upstream.SourceText[i];
+            Match m = labelRegex.Match(line);
+            if(m.Success) {
+                symbolTable[m.Groups["label"].Value] = i;
+            }
+        }
+        return new() {
+            SourceText = upstream.SourceText,
+            SymbolTable = symbolTable
+        };
+    }
+
+
+    public AssembleStage AssembleInstructions(SymbolReadingStage upstream) {
+        //upstream.
+
+        return new();
     }
 
     public byte[] Assemble(string code) {
@@ -82,9 +241,9 @@ public partial class MipsAssembler {
 
                 // if any of the parts is equal of any label
                 string? label = Array.Find(parts, labels.ContainsKey);
-                if(label is not null) {
+                if (label is not null) {
                     int value = labels[label];
-                    value = (value - (int)(codeStart + (ulong)((i+1) * 4))) >> 2;
+                    value = (value - (int)(codeStart + (ulong)((i + 1) * 4))) >> 2;
                     int partIndex = Array.IndexOf(parts, label); // poderiamos fazer uma busca so em vez de 2
                     parts[partIndex] = value.ToString();
                 }
@@ -98,10 +257,10 @@ public partial class MipsAssembler {
             var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
             for (int j = 0; j < parts.Length; j++) {
                 if (parts[j][0] == '$' && !char.IsDigit(parts[j][1])) {
-                    parts[j] = "$" + TranslateRegisterName(parts[j][1..].Replace(",",""));
+                    parts[j] = "$" + TranslateRegisterName(parts[j][1..].Replace(",", ""));
                 }
             }
-            lines[i] = (parts[0] + " " + string.Join(", ", parts[1..])).Replace(",,",",");
+            lines[i] = (parts[0] + " " + string.Join(", ", parts[1..])).Replace(",,", ",");
         }
 
         List<Instruction> instructions = [];
@@ -109,7 +268,7 @@ public partial class MipsAssembler {
             var line = lines[i];
             var baseInstruction = supportedInstructions.Find(x => x.IsMatch(line)) ?? throw new Exception($"Instruction not supported: {line}");
             Instruction? instruction = (Instruction?)Activator.CreateInstance(baseInstruction.GetType());
-            if(instruction is null) {
+            if (instruction is null) {
                 throw new Exception($"Could not create instance of instruction {baseInstruction.GetType().FullName}");
             }
             instruction.PopulateFromLine(line);
@@ -124,7 +283,7 @@ public partial class MipsAssembler {
             bytes.AddRange(BitConverter.GetBytes(instruction.ConvertToInt()));
         }
 
-        
+
         return bytes.ToArray();
     }
 }
