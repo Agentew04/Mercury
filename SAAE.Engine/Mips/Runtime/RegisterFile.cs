0e535981b24803c53672c25ce9168da07564b01a
Rodrigo Appelt <rappelt@inf.ufsm.br>
2024-08-22 19:52:37 -0300

    starting impl for mips runtime

---
+++ SAAE.Engine/Mips/Runtime/RegisterFile.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAAE.Engine.Mips.Runtime; 
+public class RegisterFile {
+
+    private List<int> _registers;
+
+    public RegisterFile() {
+        _registers = new List<int>((int)Register.COUNT);
+    }
+
+    public int Get(Register register) => Get((int)register);
+
+    public int Get(int index) {
+        if (index < 0 || index >= (int)Register.COUNT) {
+            throw new ArgumentOutOfRangeException(nameof(index), "Register index out of bounds");
+        }
+        return _registers[index];
+    }
+
+    public void Set(Register register, int value) => Set((int)register, value);
+
+    public void Set(int index, int value) {
+        if (index < 0 || index >= (int)Register.COUNT) {
+            throw new ArgumentOutOfRangeException(nameof(index), "Register index out of bounds");
+        }
+        _registers[index] = value;
+    }
+
+    public void Reset() {
+        _registers = new List<int>((int)Register.COUNT);
+    }
+
+    public int this[int index] {
+        get => Get(index);
+        set => Set(index, value);
+    }
+
+    public int this[Register register] {
+        get => Get(register);
+        set => Set(register, value);
+    }
+
+    public enum Register {
+        Zero = 0,
+        At,
+        V0,
+        V1,
+        A0,
+        A1,
+        A2,
+        A3,
+        T0,
+        T1,
+        T2,
+        T3,
+        T4,
+        T5,
+        T6,
+        T7,
+        S0,
+        S1,
+        S2,
+        S3,
+        S4,
+        S5,
+        S6,
+        S7,
+        T8,
+        T9,
+        K0,
+        K1,
+        Gp,
+        Sp,
+        Fp,
+        Ra,
+        Pc,
+        Hi,
+        Lo,
+        COUNT
+    }
+}
---
+++ SAAE.Engine/Mips/Runtime/Simple/Monocycle.cs
@@ -0,0 +1,59 @@
+﻿using SAAE.Engine.Memory;
+using SAAE.Engine.Mips.Assembler;
+using SAAE.Engine.Mips.Instructions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAAE.Engine.Mips.Runtime.Simple;
+
+/// <summary>
+/// A simplified version of the monocycle MIPS processor.
+/// Does not simulate every component of the processor.
+/// </summary>
+public class Monocycle : IClockable
+{
+    public Monocycle() {
+        // create virtual memory
+        ulong gb = 1024 * 1024 * 1024;
+        memory = new VirtualMemory(new VirtualMemoryConfiguration() {
+            ColdStoragePath = "memory.bin",
+            ColdStorageOptimization = true,
+            PageSize = 4096,
+            Size = 4*gb,
+            MaxLoadedPages = 64
+        });
+    }
+
+    /// <summary>
+    /// Represents the RAM memory
+    /// </summary>
+    private VirtualMemory memory;
+
+    /// <summary>
+    /// Structure that holds all of the general purpose
+    /// registers of the CPU.
+    /// </summary>
+    private RegisterFile registerFile = new();
+
+    private InstructionFactory instructionFactory = new();
+
+    public void Clock()
+    {
+        // read instruction from PC
+        int instructionBinary = memory.ReadWord((ulong)registerFile[RegisterFile.Register.Pc]);
+
+        // decode
+        Instruction instruction = instructionFactory.Disassemble(instructionBinary);
+
+        Execute(instruction);
+
+        // update PC
+    }
+
+    private void Execute(Instruction instruction) {
+
+    }
+}
