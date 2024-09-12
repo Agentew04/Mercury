4dd37b3b96ea41f1ff3f8128ba4aa1adab8f095b
Rodrigo Appelt <rappelt@inf.ufsm.br>
2024-09-11 21:21:52 -0300

    create editor project

---
+++ SAAE.Editor/MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace SAAE.Editor {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
