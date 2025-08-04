c05ac3c68308a4f8cf34bb967e7fcc6283461cf9
Rodrigo Appelt <rappelt@inf.ufsm.br>
2025-08-04 08:38:09 -0300

    remove instruction creator and console proj

--- InstructionCreator/Instruction/Parsing/ParseElement.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InstructionCreator.Instruction.Parsing;
-
-/// <summary>
-/// Interface comum a todos elementos que podem aparecer na 
-/// classe <see cref="ParseInfo"/> de uma instrucao.
-/// </summary>
-#pragma warning disable S2094 // Classes should not be empty
-public abstract class ParseElement { }
-#pragma warning restore S2094 // Classes should not be empty
--- InstructionCreator/Serialize.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InstructionCreator {
-    public record struct Serialize(string Type, bool Fixed, int Size, string Value, bool IsBinaryValue);
-}
