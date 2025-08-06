4246ba703bc471211d3bf3590d51fe0343b7ac44
Rodrigo Appelt <rappelt@inf.ufsm.br>
2025-08-06 19:05:03 -0300

    feat: add syntax highlighting #10

--- SAAE.Editor/Services/GrammarService.cs
+++ SAAE.Editor/Services/GrammarService.cs
@@ -1,5 +1,64 @@
-﻿namespace SAAE.Editor.Services;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using AvaloniaEdit.Highlighting;
+using AvaloniaEdit.Highlighting.Xshd;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SAAE.Editor.Models;
+using SAAE.Engine;
+
+namespace SAAE.Editor.Services;
 
 public class GrammarService : BaseService<GrammarService> {
+
+    private readonly ProjectService projectService = App.Services.GetRequiredService<ProjectService>();
+    private readonly Assembly assembly;
+
+    public GrammarService() {
+        assembly = Assembly.GetExecutingAssembly();
+    }
+
+    private Dictionary<Architecture, IHighlightingDefinition> highlightCache = [];
+    
+    public IHighlightingDefinition? GetCurrentAssemblyHighlighting() {
+        ProjectFile? project = projectService.GetCurrentProject();
+        if (project is null) {
+            Logger.LogWarning("Could not find project loaded to get assembly highlighting");
+            return null;
+        }
+
+        if (highlightCache.TryGetValue(project.Architecture, out IHighlightingDefinition? cachedHighlight)) {
+            return cachedHighlight;
+        }
+
+        Stream? s;
+        switch (project.Architecture) {
+            case Architecture.Mips:
+                s = assembly.GetManifestResourceStream("SAAE.Editor.Assets.Grammars.mips.xshd");
+                break;
+            case Architecture.Arm:
+            case Architecture.RiscV:
+            case Architecture.Unknown:
+            default:
+                Logger.LogWarning("Tried to fetch unsupported syntax highlighting: {arch}", project.Architecture);
+                return null;
+        }
+        
+        if(s is null) {
+            return null;
+        }
+
+        StreamReader sr = new(s, leaveOpen:true);
+        Console.WriteLine(sr.ReadToEnd());
+        sr.Dispose();
+        s.Position = 0;
+        using var reader = XmlReader.Create(s);
+        IHighlightingDefinition? highlight = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+        highlightCache[project.Architecture] = highlight;
+        return highlight;
+    }
     
 }
--- SAAE.Editor/Views/CodeView/FileEditorView.axaml.cs
+++ SAAE.Editor/Views/CodeView/FileEditorView.axaml.cs
@@ -1,19 +1,28 @@
-﻿using Avalonia.Controls;
+﻿using System.Xml;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using AvaloniaEdit.Highlighting;
+using AvaloniaEdit.Highlighting.Xshd;
 using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.Extensions.DependencyInjection;
 using SAAE.Editor.Models.Messages;
+using SAAE.Editor.Services;
 using SAAE.Editor.ViewModels;
 using FileEditorViewModel = SAAE.Editor.ViewModels.Code.FileEditorViewModel;
 
 namespace SAAE.Editor.Views.CodeView;
 
 public partial class FileEditorView : UserControl {
+
+    private readonly GrammarService grammarService = App.Services.GetRequiredService<GrammarService>();
+    
     public FileEditorView() {
         InitializeComponent();
         DataContext = ViewModel = App.Services.GetRequiredService<FileEditorViewModel>();
         ViewModel.TextEditor = TextEditor; // HACK
         WeakReferenceMessenger.Default.Register<FileOpenMessage>(this, OnFileOpen);
     }
+    
 
     private static void OnFileOpen(object recipient, FileOpenMessage message) {
         FileEditorView view = (FileEditorView)recipient;
@@ -22,4 +31,8 @@ public partial class FileEditorView : UserControl {
     }
 
     public FileEditorViewModel ViewModel { get; set; }
+
+    private void Control_OnLoaded(object? sender, RoutedEventArgs e) {
+        TextEditor.SyntaxHighlighting = grammarService.GetCurrentAssemblyHighlighting();
+    }
 }
