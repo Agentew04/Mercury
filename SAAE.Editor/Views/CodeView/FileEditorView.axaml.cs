851a5e2e59f1687aa44f5f0e53841d759359b6da
Rodrigo Appelt <rappelt@inf.ufsm.br>
2025-09-17 13:50:43 -0300

    feat: create crude autocomplete for directives

--- SAAE.Editor/Views/CodeView/FileEditorView.axaml.cs
+++ SAAE.Editor/Views/CodeView/FileEditorView.axaml.cs
@@ -1,11 +1,14 @@
-﻿using System.Xml;
+﻿using System.Collections.Generic;
+using System.Xml;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using AvaloniaEdit.CodeCompletion;
 using AvaloniaEdit.Highlighting;
 using AvaloniaEdit.Highlighting.Xshd;
 using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.Extensions.DependencyInjection;
+using SAAE.Editor.Models;
 using SAAE.Editor.Models.Messages;
 using SAAE.Editor.Services;
 using SAAE.Editor.ViewModels;
@@ -23,6 +26,8 @@ public partial class FileEditorView : UserControl {
         DataContext = ViewModel = App.Services.GetRequiredService<FileEditorViewModel>();
         ViewModel.SetView(this);
         WeakReferenceMessenger.Default.Register<FileOpenMessage>(this, OnFileOpen);
+        TextEditor.TextArea.TextEntering += TextEntering;
+        TextEditor.TextArea.TextEntered += TextEntered;
     }
     
 
@@ -33,6 +38,7 @@ public partial class FileEditorView : UserControl {
     }
 
     public FileEditorViewModel ViewModel { get; set; }
+    private CompletionWindow? completionWindow;
 
     private void Control_OnLoaded(object? sender, RoutedEventArgs e) {
         TextEditor.SyntaxHighlighting = grammarService.GetCurrentAssemblyHighlighting();
@@ -43,6 +49,37 @@ public partial class FileEditorView : UserControl {
         if (!point.Properties.IsMiddleButtonPressed) return;
         if ((sender as Control)?.DataContext is OpenFile file) {
             ViewModel.CloseTabCommand.Execute(file);
+        }
+    }
+    
+    private void TextEntering(object? sender, TextInputEventArgs e) {
+        if (e.Text == ".") {
+            completionWindow = new CompletionWindow(TextEditor.TextArea);
+            IList<ICompletionData>? data = completionWindow.CompletionList.CompletionData;
+            data.Add(new TextCompletionData(".macro"));
+            data.Add(new TextCompletionData(".text"));
+            data.Add(new TextCompletionData(".data"));
+            data.Add(new TextCompletionData(".ascii"));
+            data.Add(new TextCompletionData(".asciiz"));
+            data.Add(new TextCompletionData(".word"));
+            data.Add(new TextCompletionData(".space"));
+            data.Add(new TextCompletionData(".globl"));
+            data.Add(new TextCompletionData(".align"));
+            data.Add(new TextCompletionData(".float"));
+            data.Add(new TextCompletionData(".double"));
+            data.Add(new TextCompletionData(".endmacro"));
+            completionWindow.Show();
+            completionWindow.Closed += delegate {
+                completionWindow = null;
+            };
+        }
+    }
+
+    private void TextEntered(object? sender, TextInputEventArgs e) {
+        if(e.Text?.Length > 0 && completionWindow != null) {
+            if(!char.IsLetterOrDigit(e.Text[0])) {
+                //completionWindow.CompletionList.RequestInsertion(e);
+            }
         }
     }
 }
